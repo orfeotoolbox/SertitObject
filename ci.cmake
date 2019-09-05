95495920c66521488851e1f4e17e68e5f3643bb5
Guillaume Pasero <guillaume.pasero@c-s.fr>
2019-09-06 01:22:39 +0200

    CI: clean verbosity

--- ci.cmake
+++ ci.cmake
@@ -116,41 +116,22 @@ set(RM_DATA_REF develop)
 if (RM_GET_FULL_DATA OR RM_DATA_PATTERNS)
   message(STATUS "Retrieve data files from OTB")
   execute_process(COMMAND git clone -b ${RM_DATA_REF} --depth 1 -n https://gitlab.orfeo-toolbox.org/orfeotoolbox/otb.git
-                  WORKING_DIRECTORY ${REMOTE_MODULE_SOURCE_DIR}
-                  RESULT_VARIABLE _return_code)
-  message("git clone return: ${_return_code}")
+                  WORKING_DIRECTORY ${REMOTE_MODULE_SOURCE_DIR})
   if(RM_GET_FULL_DATA)
     message(STATUS "  Get full data")
   else()
     string(REPLACE ";" "," lfs_includes "${RM_DATA_PATTERNS}")
     message(STATUS "  Get paths: ${RM_DATA_PATTERNS}")
-    message("lfs_includes : ${lfs_includes}")
     execute_process(COMMAND git config lfs.fetchinclude "${lfs_includes}"
-                    WORKING_DIRECTORY ${REMOTE_MODULE_SOURCE_DIR}/otb
-                    RESULT_VARIABLE _return_code)
-    message("git config return: ${_return_code}")
-    file(READ "otb/.git/config" _otb_gitconfig)
-    message("OTB git config :\n${_otb_gitconfig}")
+                    WORKING_DIRECTORY ${REMOTE_MODULE_SOURCE_DIR}/otb)
   endif()
   execute_process(COMMAND git lfs fetch
-                  WORKING_DIRECTORY ${REMOTE_MODULE_SOURCE_DIR}/otb
-                  RESULT_VARIABLE _return_code)
-  message("git lfs fetch return: ${_return_code}")
+                  WORKING_DIRECTORY ${REMOTE_MODULE_SOURCE_DIR}/otb)
   execute_process(COMMAND git reset HEAD *
                   WORKING_DIRECTORY ${REMOTE_MODULE_SOURCE_DIR}/otb
-                  OUTPUT_QUIET
-                  RESULT_VARIABLE _return_code)
-  message("git reset return: ${_return_code}")
+                  OUTPUT_QUIET)
   execute_process(COMMAND git checkout -- Data/*
-                  WORKING_DIRECTORY ${REMOTE_MODULE_SOURCE_DIR}/otb
-                  RESULT_VARIABLE _return_code)
-  message("git checkout return: ${_return_code}")
-  # DEBUG
-  foreach(_filename ${RM_DATA_PATTERNS})
-    file(MD5 "otb/${_filename}" _out_md5)
-    message("File [otb/${_filename}] : md5=${_out_md5}")
-  endforeach()
-
+                  WORKING_DIRECTORY ${REMOTE_MODULE_SOURCE_DIR}/otb)
   set( CONFIGURE_OPTIONS "${CONFIGURE_OPTIONS}-DOTB_DATA_ROOT:PATH=${REMOTE_MODULE_SOURCE_DIR}/otb/Data;")
 endif()
 
